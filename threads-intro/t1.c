f98426711c858a0b7a8945269845db200d9f5294
Brodie Heywood <brodieheywood@protonmail.ch>
2020-10-14 23:11:50 -0700

    Update t1.c
    Update to match t1.c (Figure 26.6) in OSTEP.

--- threads-intro/t1.c
+++ threads-intro/t1.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
-
 #include "common.h"
 #include "common_threads.h"
 
-int max;
-volatile int counter = 0; // shared global variable
+static volatile int counter = 0;
 
-void *mythread(void *arg) {
-    char *letter = arg;
-    int i; // stack (private per thread) 
-    printf("%s: begin [addr of i: %p]\n", letter, &i);
-    for (i = 0; i < max; i++) {
-	counter = counter + 1; // shared: only one
-    }
-    printf("%s: done\n", letter);
-    return NULL;
-}
-                                                                             
-int main(int argc, char *argv[]) {                    
-    if (argc != 2) {
-	fprintf(stderr, "usage: main-first <loopcount>\n");
-	exit(1);
-    }
-    max = atoi(argv[1]);
-
-    pthread_t p1, p2;
-    printf("main: begin [counter = %d] [%x]\n", counter, 
-	   (unsigned int) &counter);
-    Pthread_create(&p1, NULL, mythread, "A"); 
-    Pthread_create(&p2, NULL, mythread, "B");
-    // join waits for the threads to finish
-    Pthread_join(p1, NULL); 
-    Pthread_join(p2, NULL); 
-    printf("main: done\n [counter: %d]\n [should: %d]\n", 
-	   counter, max*2);
-    return 0;
+//
+// mythread()
+//
+// Simply adds 1 to counter repeatedly, in a loop
+// No, this is not how you would add 10,000,000 to
+// a counter, but it shows the problem nicely.
+//
+void*mythread(void*arg) {
+	printf("%s: begin\n", (char*) arg);
+	int i;
+	for (i = 0; i < 1e7; i++) {
+		counter = counter + 1;
+	}
+	printf("%s: done\n", (char*) arg);
+	return NULL;
 }
 
+//
+// main()
+//
+// Just launches two threads (pthread_create)
+// and then waits for them (pthread_join)
+//
+int main(int argc, char*argv[]) {
+	pthread_t p1, p2;
+	printf("main: begin (counter = %d)\n", counter);
+	Pthread_create(&p1, NULL, mythread, "A");
+	Pthread_create(&p2, NULL, mythread, "B");
+	
+	// join waits for the threads to finish
+	Pthread_join(p1, NULL);
+	Pthread_join(p2, NULL);
+	printf("main: done with both (counter = %d)\n", counter);
+	return 0;
+}
